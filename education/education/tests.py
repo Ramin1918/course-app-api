ac5fdbdd831993fa91b0007290314f4751475622
ramin <eminli.ramin@icloud.com>
2023-12-07 22:43:49 +0100

    7

--- education/education/tests.py
+++ education/education/tests.py
@@ -4,7 +4,6 @@ Sample test
 from django.test import SimpleTestCase
 from education import calc
 
-
 class calcTests(SimpleTestCase):
     """ Test the calc module """
     def test_add_numbers(self):
@@ -12,8 +11,7 @@ class calcTests(SimpleTestCase):
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
 
-
     def test_substract_numbers(self):
-        """Test substracting numbers """
+        """Test substracting numbers"""
         res = calc.substract(5, 6)
         self.assertEqual(res, 1)
