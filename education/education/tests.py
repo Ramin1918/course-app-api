cb88ecaa30fd30ecbd6e2bce0a0597ca03762f73
ramin <eminli.ramin@icloud.com>
2023-12-07 22:33:44 +0100

    5

--- education/education/tests.py
+++ education/education/tests.py
@@ -9,12 +9,11 @@ class calcTests(SimpleTestCase):
     """ Test the calc module """
     def test_add_numbers(self):
         """Test adding numbers together"""
-        res = calc.add(5, 6)
-        
+        res = calc.add(5, 6) 
         self.assertEqual(res, 11)
+        
         
     def test_substract_numbers(self):
         """Test substracting numbers """
         res = calc.substract(5, 6)
-        
         self.assertEqual(res, 1)
