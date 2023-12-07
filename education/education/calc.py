82afb04d5c58b3006248bde7b32caa000408a0ea
ramin <eminli.ramin@icloud.com>
2023-12-07 21:45:41 +0100

    Configuring Database Settings

---
+++ education/education/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator funtions.
+"""
+
+def add(x, y):
+    """ Add x and y and return result."""
+    return x + y
+
+def substract(x, y):
+    """ Substract x and y and return result."""
+    return y - x
---
+++ education/education/tests.py
@@ -0,0 +1,19 @@
+"""
+Sample test
+"""
+from django.test import SimpleTestCase
+from education import calc
+
+class calcTests(SimpleTestCase):
+    """ Test the calc module """
+    def test_add_numbers(self):
+        """Test adding numbers together"""
+        res = calc.add(5, 6)
+        
+        self.assertEqual(res, 11)
+        
+    def test_substract_numbers(self):
+        """Test substracting numbers """
+        res = calc.substract(5, 6)
+        
+        self.assertEqual(res, 1)
