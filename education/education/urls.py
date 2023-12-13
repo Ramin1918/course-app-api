cb0faac6960e55df6d18a0746bcd8d50c5dcb1a5
ramin <eminli.ramin@icloud.com>
2023-12-13 22:37:07 +0100

    Building User API

--- education/education/urls.py
+++ education/education/urls.py
@@ -20,7 +20,7 @@ from drf_spectacular.views import (
 )
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,5 +29,7 @@ urlpatterns = [
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs',
-    )
+    ),
+    path('api/user/', include('user.urls')),
+    # path('', include('user.urls')),
 ]
